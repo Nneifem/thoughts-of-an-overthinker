e773c5984d91849dac2bab5c57fd082a97c9b2b0
Nneif <nneifemakpunonu@gmail.com>
2023-11-01 19:47:27 -0400

    did some basic layout of client folder and working on the models

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Homepage from './pages/Homepage'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />
+      },
+      {
+        index: '/signup',
+        element: <Signup />
+      },
+      {
+        index: '/login',
+        element: <Login />
+      },
+    ],
+  },
+])
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
---
+++ server/models/Account.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const accountSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/.+@.+\..+/, 'Email must be valid']
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+    },
+});
+
+const Account = model('Account', accountSchema);
+
+module.exports = Account;
